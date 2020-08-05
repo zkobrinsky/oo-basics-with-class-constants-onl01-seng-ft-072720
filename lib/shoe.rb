7e9488f80f1e0887a22ca62fe71f3e591ca1a82f
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-05 16:57:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRAND = []
+  BRANDs = []
 
   def initialize(brand)
     @brand = brand
@@ -10,7 +10,7 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    BRAND << brand
+    BRANDS << brand
   end
 
   def cobble
